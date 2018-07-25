b15ff7c480b6292f36df29f44b633c7809ed2fcd
Clement-Jean <clement.jean@epitech.eu>
2018-07-26 03:48:39 +0800

    Apkg file class

---
+++ ApkgFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnkiSharp
+{
+    public class ApkgFile
+    {
+        private string _path;
+
+        public string Path()
+        {
+            return _path;
+        }
+
+        public ApkgFile(string path)
+        {
+            if (path.Contains(".apkg") == false)
+                throw new Exception("Need apkg file");
+            if (File.Exists(path) == false)
+                throw new Exception("Need existing file");
+
+            _path = path;
+        }
+    }
+}
