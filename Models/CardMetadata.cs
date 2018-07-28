95466bd8ad9e7506e932632b1fc50807405131bb
Clement-Jean <clement.jean@epitech.eu>
2018-07-28 23:47:47 +0800

    New model CardMetadata

---
+++ Models/CardMetadata.cs
@@ -0,0 +1,16 @@
+﻿namespace AnkiSharp.Models
+{
+    public struct CardMetadata
+    {
+        public double type;
+        public double queue;
+        public double due;
+        public double ivl;
+        public double factor;
+        public double reps;
+        public double lapses;
+        public double left;
+        public double odue;
+        public double odid;
+    }
+}
