0c3fdcba5679e993c619d69eafef57865f2ddbd1
Clement-Jean <clement.jean@epitech.eu>
2018-07-29 03:34:02 +0800

    Add new model RevLogMetadata

---
+++ Models/RevLogMetadata.cs
@@ -0,0 +1,15 @@
+﻿namespace AnkiSharp.Models
+{
+    struct RevLogMetadata
+    {
+        public double id;
+        public double cid;
+        public double usn;
+        public double ease;
+        public double ivl;
+        public double lastivl;
+        public double factor;
+        public double time;
+        public double type;
+    }
+}
