ffe95d60c9f9b8ef0f9e481d73d765bb87ecaebd
Clement-Jean <clement.jean@epitech.eu>
2018-07-30 03:31:09 +0800

    Create mapper as singleton and dynamic object

---
+++ Helpers/Mapper.cs
@@ -0,0 +1,49 @@
+﻿
+using AnkiSharp.Models;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace AnkiSharp.Helpers
+{
+    internal class Mapper
+    {
+        private static Mapper instance = null;
+
+        private Mapper()
+        {
+        }
+
+        public static Mapper Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Mapper();
+                }
+                return instance;
+            }
+        }
+        
+        public static List<AnkiSharpDynamic> MapSQLiteReader(SQLiteConnection conn, string toExecute)
+        {
+            List<AnkiSharpDynamic> result = new List<AnkiSharpDynamic>();
+            SQLiteDataReader reader = SQLiteHelper.ExecuteSQLiteCommandRead(conn, toExecute);
+
+            while (reader.Read())
+            {
+                AnkiSharpDynamic ankiSharpDynamic = new AnkiSharpDynamic();
+
+                for (int i = 0; i < reader.FieldCount; ++i)
+                {
+                    ankiSharpDynamic[reader.GetName(i)] = reader.GetValue(i);
+                }
+
+                result.Add(ankiSharpDynamic);
+            }
+
+            reader.Close();
+            return result;
+        }
+    }
+}
---
+++ Models/AnkiSharpDynamic.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection;
+
+namespace AnkiSharp.Models
+{
+    public class AnkiSharpDynamic : DynamicObject
+    {
+        #region FIELDS
+        Dictionary<string, object> _dictionary = new Dictionary<string, object>();
+        #endregion
+
+        #region PROPERTIES
+        public object this[string elem]
+        {
+            get { return _dictionary[elem]; }
+            set { _dictionary[elem] = value; }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _dictionary.Count;
+            }
+        }
+        #endregion
+
+        #region CTOR
+        public AnkiSharpDynamic()
+        {
+        }
+        #endregion
+
+        #region FUNCTIONS
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            string name = binder.Name.ToLower();
+
+            return _dictionary.TryGetValue(name, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            _dictionary[binder.Name.ToLower()] = value;
+
+            return true;
+        }
+
+        public static bool operator==(AnkiSharpDynamic first, AnkiSharpDynamic second)
+        {
+            foreach (var pair in first._dictionary)
+            {
+                if (second._dictionary[pair.Key].ToString() != pair.Value.ToString())
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool operator!=(AnkiSharpDynamic first, AnkiSharpDynamic second)
+        {
+            return !(first == second);
+        }
+
+        public T ToObject<T>() where T : new()
+        {
+            T result = new T();
+            PropertyInfo propertyInfo;
+
+            foreach (var pair in _dictionary)
+            {
+                    propertyInfo = result.GetType().GetProperty(pair.Key);
+                    propertyInfo.SetValue(result, Convert.ChangeType(pair.Value, propertyInfo.PropertyType), null);
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
