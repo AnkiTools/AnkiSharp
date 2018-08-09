977531bc45f7889c6ff15481872da481d8b9b78b
Clement-Jean <clement.jean@epitech.eu>
2018-08-10 03:21:03 +0800

    CreateAudio function with synthetizer

---
+++ Helpers/SynthetizerHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Media;
+using System.Speech.AudioFormat;
+using System.Speech.Synthesis;
+
+namespace AnkiSharp.Helpers
+{
+    internal static class SynthetizerHelper
+    {
+        internal static void CreateAudio(string path, string text, CultureInfo cultureInfo)
+        {
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+                synth.SetOutputToWaveFile(path,
+                  new SpeechAudioFormatInfo(32000, AudioBitsPerSample.Sixteen, AudioChannel.Mono));
+
+                PromptBuilder builder = new PromptBuilder(cultureInfo);
+                builder.AppendText(text);
+                
+                synth.Speak(builder);
+            }
+        }
+
+    }
+}
