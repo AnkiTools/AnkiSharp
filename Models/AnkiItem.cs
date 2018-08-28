2779c79ea706c42d8fd1507ae9e996c84a7d6c7f
Clement-Jean <clement.jean@epitech.eu>
2018-08-29 04:39:53 +0800

    change namespace for accessibility

--- Models/AnkiItem.cs
+++ Models/AnkiItem.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using AnkiSharp.Models;
+using System.Collections.Generic;
 using System.Dynamic;
 
-namespace AnkiSharp.Models
+namespace AnkiSharp
 {
     public class AnkiItem : DynamicObject
     {
